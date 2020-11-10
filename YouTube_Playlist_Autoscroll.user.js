c3e9f16e53c22e334f771f0039081a882435e5b3
Technetium <Technetium1@users.noreply.github.com>
2020-11-09 21:40:32 -0500

    Create YouTube_Playlist_Autoscroll.user.js

---
+++ YouTube_Playlist_Autoscroll.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         YouTube Playlist Autoscroll
+// @version      1.0
+// @description  Automatically scroll to the bottom of your YouTube playlists
+// @author       https://github.com/Technetium1
+// @license      The Unlicense
+// @match        https://www.youtube.com/playlist*
+// @updateURL    https://github.com/Technetium1/Userscripts/raw/master/YouTube_Playlist_Autoscroll.user.js
+// @downloadURL  https://github.com/Technetium1/Userscripts/raw/master/YouTube_Playlist_Autoscroll.user.js
+// @run-at       document-body
+// ==/UserScript==
+
+window.onload = function(){
+  setInterval(autoScroll, 100);
+}
+
+function autoScroll(){
+  if(document.getElementsByClassName("paper-spinner").length>0){
+  window.scrollBy({
+    top: 1000000,
+    behavior: 'smooth'});
+  };
+
+};
