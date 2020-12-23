ffd8f7d8fb6306e3ba1b3df373116c51b7ebe229
Technetium <Technetium1@users.noreply.github.com>
2020-12-23 10:59:01 -0500

    v1.4
    I wish I knew why this helps it work more reliably. May need to explicitly exclude pages in the future if it breaks

--- YouTube_Playlist_Autoscroll.user.js
+++ YouTube_Playlist_Autoscroll.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         YouTube Playlist Autoscroll
-// @version      1.3
+// @version      1.4
 // @description  Automatically scroll to the bottom of your YouTube playlists
 // @author       https://github.com/Technetium1
 // @license      The Unlicense
-// @match        https://www.youtube.com/playlist*
+// @match        https://www.youtube.com/*
+// @exclude      https://www.youtube.com/
 // @updateURL    https://github.com/Technetium1/Userscripts/raw/main/YouTube_Playlist_Autoscroll.user.js
 // @downloadURL  https://github.com/Technetium1/Userscripts/raw/main/YouTube_Playlist_Autoscroll.user.js
-// @run-at       document-body
+// @run-at       document-start
 // ==/UserScript==
 
 window.onload = function () {
